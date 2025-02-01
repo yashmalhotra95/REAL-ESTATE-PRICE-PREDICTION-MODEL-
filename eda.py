e87511e513a5c1f21b602a92fe97932b8684ed85
Yash Malhotra <128459293+yashmalhotra95@users.noreply.github.com>
2025-02-02 00:43:47 +0530

    Create eda.py

---
+++ eda.py
@@ -0,0 +1,5 @@
+## 1.EDA
+df.isnull().any()
+There are NAN values in columns 'exterior walls', 'roof' and 'basement'.
+The correlation matrix shows the correlation between each features. From the 'tx_price' column, we can see that less then 6 features have a correlation higher than 0.4 with the transaction price.
+df_numeric = df.select_dtypes(include=[np.number])
