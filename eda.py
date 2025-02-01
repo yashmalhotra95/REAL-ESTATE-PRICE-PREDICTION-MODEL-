bba7462bd755d75aee11b0b80df623e04b6ede1f
Yash Malhotra <128459293+yashmalhotra95@users.noreply.github.com>
2025-02-02 00:54:49 +0530

    Update eda.py

--- eda.py
+++ eda.py
@@ -1,14 +1,3 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.model_selection import train_test_split
-from mpl_toolkits.mplot3d import Axes3D
-%matplotlib inline
-df = pd.read_csv("/content/real_estate_data.csv")
-print ("Shape of dataframe: ",df.shape)
-df = df.reset_index() #as there will be
-df.head(5)
 ## 1.EDA
 df.isnull().any()
 # There are NAN values in columns 'exterior walls', 'roof' and 'basement'.
