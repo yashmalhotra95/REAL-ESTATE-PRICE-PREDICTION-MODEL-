a5cc08c3ce3be5a2cf9ee85e847cc9b9de4e7ef3
Yash Malhotra <128459293+yashmalhotra95@users.noreply.github.com>
2025-02-02 01:03:19 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -1,10 +1,14 @@
 ## 2. Feature Engineering
+
 # add in house age
 df["House Age"] = df.tx_year - df.year_built
+
 # add in price/sqft
 df["PriceSqft"] = df.tx_price / df.sqft
+
 # add monthly fixed expenses
 df["Expenses"] = df.property_tax + df.insurance
+
 fig, ax = plt.subplots(figsize=(10,10))
 ax2 = ax.twinx()
 ax.plot(df.PriceSqft.groupby(by=df.tx_year).mean().index,df.PriceSqft.groupby(by=df.tx_year).mean().values)
@@ -16,6 +20,7 @@ ax2.set_ylabel("Mean Housing price")
 ax.legend(['Housing Prices/Sqft'],loc=1)
 ax2.legend(['Mean Housing price'],loc=2)
 <matplotlib.legend.Legend at 0x7b2e0bc0bdf0>
+
 fig, ax = plt.subplots(figsize=(10,10))
 ax2 = ax.twinx()
 ax.plot(df.PriceSqft.groupby(by=df.tx_year).mean().index,df.PriceSqft.groupby(by=df.tx_year).mean().values)
